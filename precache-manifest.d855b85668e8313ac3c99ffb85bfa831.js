d5ec60781b7b212d1f8bf7fe36276d81f176f346
Ankit <ankitphondani52@gmail.com>
2020-04-17 01:21:35 +0530

    Updates

---
+++ precache-manifest.d855b85668e8313ac3c99ffb85bfa831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab272ff89bc20ad0989100937178240",
+    "url": "/corona_dashboard/index.html"
+  },
+  {
+    "revision": "866a2b52723a1d3fa700",
+    "url": "/corona_dashboard/static/css/main.8fb01499.chunk.css"
+  },
+  {
+    "revision": "030dc10a5a9734e869de",
+    "url": "/corona_dashboard/static/js/2.29a9d721.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/corona_dashboard/static/js/2.29a9d721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866a2b52723a1d3fa700",
+    "url": "/corona_dashboard/static/js/main.8637e70d.chunk.js"
+  },
+  {
+    "revision": "dbc030b85413add711ab",
+    "url": "/corona_dashboard/static/js/runtime-main.118c474e.js"
+  }
+]);
